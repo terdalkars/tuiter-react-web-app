15ac683a5bef02f496ebcd7e7865d5c2f74e8d5f
terdalkars <terdalkar.s@northeastern.edu>
2023-08-16 19:40:49 -0400

    api changed

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -36,7 +36,7 @@
 //
 
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const SERVER_API_URL = process.env.REACT_APP_API_BASE;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
